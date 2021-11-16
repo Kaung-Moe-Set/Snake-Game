2125a325bc711f34fd728d8dbf4e758c53491ae8
KaungMoeSet <kaungmoeset@gmail.com>
2021-11-16 15:11:47 +0630

    Create class and essential method

---
+++ Snake Game/src/GamePanel.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+	GamePanel() {
+		
+	}
+	
+	public void startGame() {
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollisions() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public class myKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
+}
