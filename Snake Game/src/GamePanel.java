7bc9fe62d88748b7e3b5d5da30341bdf8a6d952b
KaungMoeSet <kaungmoeset@gmail.com>
2021-11-22 13:25:44 +0630

    draw snake and move snake

--- Snake Game/src/GamePanel.java
+++ Snake Game/src/GamePanel.java
@@ -13,7 +13,7 @@ public class GamePanel extends JPanel implements ActionListener {
 	static final int delay = 75;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
-	int bodypart = 6;
+	int bodyParts = 6;
 	int applesEaten;
 	int appleX;
 	int appleY;
@@ -50,6 +50,17 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 		g.setColor(Color.green);
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+		
+		for(int i = 0; i<bodyParts; i++) {
+			if(i == 0) {
+				g.setColor(Color.red);
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			} 
+			else {
+				g.setColor(new Color(220,20,60));
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+		}
 	}
 	public void newApple() {
 		
@@ -57,7 +68,24 @@ public class GamePanel extends JPanel implements ActionListener {
 		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 	}
 	public void move() {
-		
+		for(int i = bodyParts; i>0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
+		switch(direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
 	}
 	public void checkApple() {
 		
@@ -71,7 +99,12 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 		
 	}
 
