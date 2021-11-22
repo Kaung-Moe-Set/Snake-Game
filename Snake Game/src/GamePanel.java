ed2b5c32627d418264a4c1fa3d107da0393afdf9
KaungMoeSet <kaungmoeset@gmail.com>
2021-11-22 16:24:15 +0630

    create option and reduce the unit size -5

--- Snake Game/src/GamePanel.java
+++ Snake Game/src/GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
-	static final int UNIT_SIZE = 25;
+	static final int UNIT_SIZE = 20;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
 	static final int delay = 75;
 	final int x[] = new int[GAME_UNITS];
--- Snake Game/src/Main.java
+++ Snake Game/src/Main.java
@@ -1,10 +1,47 @@
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		new GameFrame();
-		new GamePanel();
+		new Options();
+	}
+	
+}
+
+class Options implements ActionListener {
+	
+	JFrame frame = new JFrame();
+	JButton single = new JButton("Single Player");
+	JButton multi = new JButton("Multi Player");
+	
+	Options() {
+		single.setBounds(30, 100, 150, 40);
+		single.setFocusable(false);
+		single.addActionListener(this);
+		
+		multi.setBounds(200, 100, 150, 40);
+		multi.setFocusable(false);
+		multi.addActionListener(this);
+
+		frame.add(single);
+		frame.add(multi);
+		frame.setSize(420,300);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == single) {
+			frame.dispose();
+			new GameFrame();
+		}else {
+			
+		}
+		
+	}
 }
