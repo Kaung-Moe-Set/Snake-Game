826e52934b4e8e01791dd4fd13da1ff227be517f
KaungMoeSet <kaungmoeset@gmail.com>
2021-12-11 17:45:19 +0630

    left is two snake collide with each other

--- Snake Game/src/GamePanelMulti.java
+++ Snake Game/src/GamePanelMulti.java
@@ -10,21 +10,24 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 20;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-	static final int delay = 1000;
+	static final int delay = 500;
 	final int Ax[] = new int[GAME_UNITS];
 	final int Ay[] = new int[GAME_UNITS];
 	final int Bx[] = new int[GAME_UNITS];
 	final int By[] = new int[GAME_UNITS];
-	int bodyParts = 2;
+	int AbodyParts = 2;
+	int BbodyParts = 2;
 	int AapplesEaten;
 	int BapplesEaten;
 	int appleX;
 	int appleY;
 	char Adirection = 'R';
-	char Bdirection = 'L';
+	char Bdirection = 'R';
 	boolean running = false;
 	Timer timer;
 	Random random;
+	boolean condition; //to check collisions and true is for red and false is for blue
+	int position = UNIT_SIZE*29;
 	
 	GamePanelMulti(){
 		
@@ -60,7 +63,7 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 			
 			//for snakeA
-			for(int i = 0; i<bodyParts; i++) {
+			for(int i = 0; i<AbodyParts; i++) {
 				if(i == 0) {
 					g.setColor(Color.red);
 					g.fillRect(Ax[i], Ay[i], UNIT_SIZE, UNIT_SIZE);
@@ -72,20 +75,20 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 			}
 			
 			//for snakeB
-			for(int i = 0; i<bodyParts; i++) {
+			for(int i = 0; i<BbodyParts; i++) {
 				if(i == 0) {
 					g.setColor(Color.blue);
-					g.fillRect(Bx[i]+(UNIT_SIZE*29), By[i]+(UNIT_SIZE*29), UNIT_SIZE, UNIT_SIZE);
+					g.fillRect(Bx[i], By[i]+position, UNIT_SIZE, UNIT_SIZE);
 				} 
 				else {
 					g.setColor(new Color(51,153,255));
-					g.fillRect(Bx[i]+(UNIT_SIZE*29), By[i]+(UNIT_SIZE*29), UNIT_SIZE, UNIT_SIZE);
+					g.fillRect(Bx[i], By[i]+position, UNIT_SIZE, UNIT_SIZE);
 				}
 			}
 			
 			
 		}else {
-			gameOver(g);
+			gameOver(g,condition);
 		}
 		
 	}
@@ -95,7 +98,7 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 	}
 	
 	public void moveA() {
-		for(int i = bodyParts; i>0; i--) {
+		for(int i = AbodyParts; i>0; i--) {
 			Ax[i] = Ax[i - 1];
 			Ay[i] = Ay[i - 1];
 		}
@@ -115,8 +118,9 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 			break;
 		}
 	}
+	
 	public void moveB() {
-		for(int i = bodyParts; i>0; i--) {
+		for(int i = BbodyParts; i>0; i--) {
 			Bx[i] = Bx[i - 1];
 			By[i] = By[i - 1];
 		}
@@ -138,20 +142,89 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 	}
 	public void checkApple() {
 		if((Ax[0] == appleX) && (Ay[0] == appleY)) {
-			bodyParts++;
+			AbodyParts++;
 			AapplesEaten++;
 			newApple();
-		}else if((Bx[0] == appleX) && (By[0] == appleY)) {
-			bodyParts++;
+		}
+		if((Bx[0] == appleX) && (By[0] == appleY)) {
+			BbodyParts++;
 			BapplesEaten++;
 			newApple();
 		}
 	}
 	public void checkCollisions() {
 		
-	}
-	public void gameOver(Graphics g) {
-		if(AapplesEaten > BapplesEaten ) {
+		//For snake RED
+		for( int i=AbodyParts; i>0; i--) {
+			//check if head collides with body
+			if((Ax[0] == Ax[i]) && (Ay[0] == Ay[i])) {
+				running = false;
+				condition = false;
+				}
+			}
+		//check if head touches the left border
+			if(Ax[0] < 0) {
+				running = false;
+				condition = false;
+			}
+		//check if head touches the right border
+			if((Ax[0] > SCREEN_WIDTH)) {
+				running = false;
+				condition = false;
+			}
+		//check if head touches the top border
+			if(Ay[0] < 0) {
+				running = false;
+				condition = false;
+			}
+		//check if head touches the bottom border
+			if(Ay[0] > SCREEN_HEIGHT) {
+				running = false;
+				condition = false;
+			}		
+		
+		//___________________________________________________
+		//For snake Blue
+		for( int i=BbodyParts; i>0; i--) {
+			//check if head collides with body
+			if((Bx[0] == Bx[i]) && (By[0] == By[i])) {
+				running = false;
+				condition = true;
+			}
+		}
+		//check if head touches the left border
+		if(Bx[0] < 0) {
+			running = false;
+			condition = true;
+		}
+			
+		//check if head touches the right border
+		if((Bx[0] > SCREEN_WIDTH)) {
+			running = false;
+			condition = true;
+		}
+			
+		//check if head touches the top border
+		if(By[0] < 0-SCREEN_HEIGHT) {
+			running = false;
+			condition = true;
+		}
+			
+		//check if head touches the bottom border
+		if(By[0] > 0) {
+			running = false;
+			condition = true;
+		}		
+			
+		if(!running) {
+			timer.stop();
+			}
+			
+		}
+	
+	public void gameOver(Graphics g,boolean condition) {
+		if(condition) {
+			//Score
 			g.setColor(Color.red);
 			g.setFont( new Font("Ink Free",Font.BOLD, 40));
 			FontMetrics metrics1 = getFontMetrics(g.getFont());
@@ -159,14 +232,15 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 						(SCREEN_WIDTH - metrics1.stringWidth("Score: "+AapplesEaten))/2, 
 						g.getFont().getSize());
 			
-			g.setColor(Color.red);
-			g.setFont( new Font("Ink Free",Font.BOLD, 75));
+			//Game Over text
+			g.setColor(Color.GREEN);
+			g.setFont( new Font("Ink Free",Font.BOLD, 50));
 			FontMetrics metrics2 = getFontMetrics(g.getFont());
-			g.drawString("Game Over", 
-								(SCREEN_WIDTH - metrics2.stringWidth("RED WIN!"))/2, 
-								SCREEN_HEIGHT/2);
-			
-		}else if(BapplesEaten > AapplesEaten) {
+			g.drawString("CONGRATULATIONS!", 
+						(SCREEN_WIDTH - metrics2.stringWidth("CONGRATULATIONS"))/2, 
+						SCREEN_HEIGHT/2);
+		}else {
+			//Score
 			g.setColor(Color.blue);
 			g.setFont( new Font("Ink Free",Font.BOLD, 40));
 			FontMetrics metrics1 = getFontMetrics(g.getFont());
@@ -174,20 +248,13 @@ public class GamePanelMulti extends JPanel implements ActionListener {
 						(SCREEN_WIDTH - metrics1.stringWidth("Score: "+BapplesEaten))/2, 
 						g.getFont().getSize());
 			
-			g.setColor(Color.red);
-			g.setFont( new Font("Ink Free",Font.BOLD, 75));
+			//Game Over text
+			g.setColor(Color.GREEN);
+			g.setFont( new Font("Ink Free",Font.BOLD, 50));
 			FontMetrics metrics2 = getFontMetrics(g.getFont());
-			g.drawString("Game Over", 
-								(SCREEN_WIDTH - metrics2.stringWidth("BLUE WIN!"))/2, 
-								SCREEN_HEIGHT/2);
-		}else {
-			//Tie condition
-			g.setColor(Color.red);
-			g.setFont( new Font("Ink Free",Font.BOLD, 75));
-			FontMetrics metrics2 = getFontMetrics(g.getFont());
-			g.drawString("Game Over", 
-								(SCREEN_WIDTH - metrics2.stringWidth("Tie"))/2, 
-								SCREEN_HEIGHT/2);
+			g.drawString("CONGRATULATIONS!", 
+						(SCREEN_WIDTH - metrics2.stringWidth("CONGRATULATIONS"))/2, 
+						SCREEN_HEIGHT/2);
 		}
 	}
 	
